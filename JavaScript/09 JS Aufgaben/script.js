881b8ba88796420a024ee688b0a1b6196ee72a06
Tiemo <viereckig.nesensohn@gmail.com>
2022-10-18 12:55:25 +0200

    09 finished

---
+++ JavaScript/09 JS Aufgaben/script.js
@@ -0,0 +1,20 @@
+//Erstelle mit Hilfe einer Schleife und Modulo das folgende Ausgabe liefert: 
+//X X X X 
+//O O O O 
+//X X X X 
+//O O O O 
+
+//2. Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.) 
+
+let size = 4;
+for (let row = 0; row < size; row++) {
+    if (row % 2 == 0) {
+        for (let column = 0; column < size; column++) {
+            process.stdout.write("X ");
+        }
+    } else {
+        for (let column = 0; column < size; column++) {
+            process.stdout.write("O ");
+        }
+    }
+}
