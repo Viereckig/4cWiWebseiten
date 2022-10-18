de4860f4be5ac809a94a49fe053ed96e14a8e0b8
Tiemo <viereckig.nesensohn@gmail.com>
2022-10-18 12:47:05 +0200

    08 finished

---
+++ JavaScript/08 JS Switch Statement/script.js
@@ -0,0 +1,26 @@
+// Erstelle eine Zufallszahl zwischen 5 und 10
+
+let zahl = Math.floor(Math.random() * 6) + 5;
+
+switch (zahl) {
+    case 5:
+        console.log("Five");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    case 9:
+        console.log("Nine");
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+}
+
+    
