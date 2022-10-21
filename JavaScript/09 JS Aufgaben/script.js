01b4083e0938426dd84357f4b1bebb4c0ecc5e4c
Tiemo <viereckig.nesensohn@gmail.com>
2022-10-21 08:57:25 +0200

    09 finished

--- JavaScript/09 JS Aufgaben/script.js
+++ JavaScript/09 JS Aufgaben/script.js
@@ -6,7 +6,7 @@
 
 //2. Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.) 
 
-let size = 4;
+let size = 5;
 for (let row = 0; row < size; row++) {
     if (row % 2 == 0) {
         for (let column = 0; column < size; column++) {
@@ -17,4 +17,5 @@ for (let row = 0; row < size; row++) {
             process.stdout.write("O ");
         }
     }
+    console.log("");
 }
