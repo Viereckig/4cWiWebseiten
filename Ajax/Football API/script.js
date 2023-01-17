1f736a8ea3c3bf817b9955cc849d65474d3e57d4
Tiemo <viereckig.nesensohn@gmail.com>
2023-01-17 12:50:40 +0100

    Football API finished

---
+++ Ajax/Football API/script.js
@@ -0,0 +1,24 @@
+const loadData = () => {
+    fetch("https://api.openligadb.de/getbltable/bl1/2022").then((result) => {
+        result.json().then((data) => {
+            console.log(data);
+            fillTable(data);
+        })
+    })
+}
+
+const fillTable = (data) => {
+    
+    let html = "";
+
+    data.forEach(element => {
+        html+="<div>" + element.points + " Points by Team " + element.teamName + "</div>"
+    });
+
+    document.getElementById("output").innerHTML=html;
+}
+
+
+
+
+loadData();
